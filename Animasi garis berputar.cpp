150fd5cba09798da1a04fb5f2a42eb8956f811a5
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-04 12:34:56 +0700

    Program C++

---
+++ Animasi garis berputar.cpp
@@ -0,0 +1,30 @@
+#include<ncurses/ncurses.h>
+#include<windows.h>
+
+int main()
+{
+	initscr();
+	while(true){
+		clear();
+		mvprintw(1,5,"|");
+		refresh();
+		Sleep(500);
+		
+		mvprintw(1,5,"/");
+		refresh();
+		Sleep(500);
+		
+		mvprintw(1,5,"-");
+		refresh();
+		Sleep(500);
+		
+		mvprintw(1,5,"\\");
+		refresh();
+		Sleep(500);
+		
+	
+		
+	}
+	getch();
+	endwin();
+}
